ec0a38127ef540c80a706316e2660a55dc07f974
prabin-7 <prabinneupane2001@gmail.com>
2023-04-25 13:02:38 +0545

    cors added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 import express from "express"
+import cors from "cors";
 
 const app = express()
 const PORT = 3000;
+
+app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      credentials: true,
+      exposedHeaders: ["set-cookie"],
+    })
+  );
 
 app.post("/", (req,res) => {
     const data = req.body
